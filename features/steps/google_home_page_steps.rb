ba7915654440974e0fe9b5119dd3569a2ee5f41a
Francisco Souza <francisco@franciscosouza.net>
2010-09-07 17:07:37 -0300

    Fixed Ruby step definition

--- features/steps/google_home_page_steps.rb
+++ features/steps/google_home_page_steps.rb
@@ -2,6 +2,7 @@ require "selenium/client"
 
 Given /^that I am on the browser start page$/ do
   @browser = Selenium::Client::Driver.new :host => "localhost", :port => 4444, :browser => "*firefox", :url => "http://www.google.com.br", :timeout_in_second => 60
+  @browser.start_new_browser_session
 end
 
 When /^I go to the website www\.google\.com\.br$/ do
